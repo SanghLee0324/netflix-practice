32d00937535cea2c25ad99406c4789b2f045b07e
SANGH-LEE\sangh <sanghlee0324@gmail.com>
2022-09-29 22:32:11 +0900

    Navigation bar 구현 및 babel-eslint 오류 수정

---
+++ src/components/Nav.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from "react";
+import "./Nav.css";
+
+export default function Nav() {
+  const { show, setShow } = useState(false);
+
+  //스크롤 시 nav 배경색 바꾸기
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 50) {
+        setShow(true);
+      } else {
+        setShow(false);
+      }
+    });
+    return () => {
+      window.removeEventListener("scroll", () => {});
+    };
+  });
+
+  return (
+    // show가 true일때만 nav_black css 적용되도록
+    <nav className={`nav ${show && "nav_black"}`}>
+      <img //넷플릭스 로고
+        alt="Netflix logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png"
+        className="nav_logo"
+        onClick={() => window.location.reload()}
+      />
+      <img //사용자 로고
+        alt="User logged"
+        src="https://pro2-bar-s3-cdn-cf1.myportfolio.com/dddb0c1b4ab622854dd81280840458d3/92995677ac0aab719760c33c.png?h=3b9eb2133e52e4c190235d60872eb1e4"
+        className="nav_avatar"
+      />
+    </nav>
+  );
+}

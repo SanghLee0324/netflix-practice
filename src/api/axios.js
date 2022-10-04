73f11ae2c0f230d56d37f66497dfc6462cb2feea
SANGH-LEE\sangh <sanghlee0324@gmail.com>
2022-10-05 00:22:20 +0900

    API KEY 숨김

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const instance = axios.create({
   baseURL: "https://api.themoviedb.org/3",
   params: {
-    api_key: "226d253fbf86f74892298aac48a4ceaf",
+    api_key: process.env.REACT_APP_MOVIE_DB_API_KEY,
     language: "ko-KR",
   },
 });

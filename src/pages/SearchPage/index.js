77747b49bf39c9005ef75a9814ea032d2bb40289
SANGH-LEE\sangh <sanghlee0324@gmail.com>
2022-10-04 19:29:08 +0900

    useDebounce Custon Hooks 만들기

--- src/pages/SearchPage/index.js
+++ src/pages/SearchPage/index.js
@@ -2,6 +2,7 @@ import axios from "../../api/axios";
 import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import "./SearchPage.css";
+import { useDebounce } from "../../hooks/useDebounce";
 
 export default function SearchPage() {
   const [searchResults, setSearchResults] = useState([]);
@@ -11,12 +12,13 @@ export default function SearchPage() {
 
   let query = useQuery();
   const searchTerm = query.get("q");
+  const debouncedSearchTerm = useDebounce(searchTerm, 500);
 
   useEffect(() => {
-    if (searchTerm) {
-      fetchSearchMovie(searchTerm);
+    if (debouncedSearchTerm) {
+      fetchSearchMovie(debouncedSearchTerm);
     }
-  }, [searchTerm]);
+  }, [debouncedSearchTerm]);
 
   const fetchSearchMovie = async searchTerm => {
     try {
@@ -37,7 +39,7 @@ export default function SearchPage() {
             const movieImageUrl =
               "https://image.tmdb.org/t/p/w500" + movie.backdrop_path;
             return (
-              <div className="movie">
+              <div className="movie" key={movie.id}>
                 <div className="movie_column-poster">
                   <img
                     src={movieImageUrl}
@@ -53,7 +55,9 @@ export default function SearchPage() {
     ) : (
       <section className="no-results">
         <div className="no-results_text">
-          <p>찾고자 하는 검색어"{searchTerm}"에 맞는 영화가 없습니다.</p>
+          <p>
+            찾고자 하는 검색어"{debouncedSearchTerm}"에 맞는 영화가 없습니다.
+          </p>
         </div>
       </section>
     );
